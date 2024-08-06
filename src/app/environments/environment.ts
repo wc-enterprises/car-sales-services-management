e734bb8b9ddb1f79b40155b8d2cb2c820e16ed5f
Abinwc <abinpaul5598@gmail.com>
2024-08-06 20:17:14 +0530

    right project config

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyCRTol9SxnJQb-1YwFFupqN6PMwV5fLqcM",
-    authDomain: "car-sales-service-manage-8e836.firebaseapp.com",
+    apiKey: "AIzaSyAmGvCTNOF0TTmZracSSBmWN-ZvG7bqJt8",
+    authDomain: "car-sales-service-management.firebaseapp.com",
     databaseURL:
-      "https://car-sales-service-manage-8e836-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "car-sales-service-manage-8e836",
-    storageBucket: "car-sales-service-manage-8e836.appspot.com",
-    messagingSenderId: "663587503227",
-    appId: "1:663587503227:web:db09eab645a779458676de",
-    measurementId: "G-WYH2B60ZL9",
+      "https://car-sales-service-management-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "car-sales-service-management",
+    storageBucket: "car-sales-service-management.appspot.com",
+    messagingSenderId: "268044674385",
+    appId: "1:268044674385:web:a6b5930ea178eed2b21f70",
   },
 };
